64bdb6002c1af72f5cf23e077fbd12eb466f2992
BeixiJia <240793032@qq.com>
2020-12-21 23:24:13 +0800

    教学工程上传

---
+++ Sliding Cube/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform followTransform;
+    public float LagSpeed;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        followTransform = PlayerController.instance.transform;
+        offset = transform.position - followTransform.position;
+    }
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, followTransform.position + offset, LagSpeed);
+    }
+}
