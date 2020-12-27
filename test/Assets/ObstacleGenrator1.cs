a1f9659e343900abc2550fd47ae96b62fa57baa2
Shaw <hyx0110@qq.com>
2020-12-27 15:56:01 +0800

    作业练习更新

---
+++ test/Assets/ObstacleGenrator1.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ObstacleGenrator1 : MonoBehaviour
+{
+    public int CreatAmount;
+    public Vector3 StartPosition;
+    public Vector3 PositionOffset;
+
+    public GameObject[] Blockprefab;
+
+    void Start()
+    {
+        for (int i = 0; i < CreatAmount; i++)
+        {
+            Vector3 position = StartPosition;
+            position = position + PositionOffset * i;
+
+            int inx = Random.Range(0, Blockprefab.Length);
+            Instantiate(Blockprefab[inx], position, Quaternion.identity);
+
+        }
+
+    }
+
+}
---
+++ test/Assets/Spiral.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Spiral : MonoBehaviour
+{
+    public Vector3 SpinEuler;
+    public Transform spinTransform;
+
+    void Update()
+    {
+        spinTransform.Rotate(SpinEuler * Time.deltaTime);
+    }
+}
