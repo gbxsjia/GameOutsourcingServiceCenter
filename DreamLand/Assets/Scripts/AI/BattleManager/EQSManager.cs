8c03ac32a9e39df16aaa0e75e214c6b0e79a4a3d
BeixiJia <240793032@qq.com>
2021-01-05 20:37:29 +0800

    EQS开始

---
+++ DreamLand/Assets/Scripts/AI/BattleManager/EQSManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EQSManager : MonoBehaviour
+{
+    private EQSProbe[,] Probes;
+    [SerializeField]
+    private GameObject ProbePrefab;
+   
+
+}
