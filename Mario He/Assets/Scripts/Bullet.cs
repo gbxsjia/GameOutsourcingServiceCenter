ab75ee1566f70ded26851f4c098115253cb5944b
Shaw <hyx0110@qq.com>
2021-02-10 17:30:49 +0800

    马里奥·何交作业了

---
+++ Mario He/Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+
+        Vector3 v = rb.velocity;
+        v = transform.forward * Speed;
+
+        rb.velocity = v;
+
+    }
+
+}
